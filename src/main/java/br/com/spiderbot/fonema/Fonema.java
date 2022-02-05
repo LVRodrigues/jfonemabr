0fbdc6ff249258e98862adb2ef704353b9b8ea9f
Luciano Vieira Rodrigues <lvrodriguesline@gmail.com>
2022-02-05 16:40:03 -0300

    Protótipos e testes unitários.

--- src/main/java/br/com/spiderbot/fonema/Fonema.java
+++ src/main/java/br/com/spiderbot/fonema/Fonema.java
@@ -9,5 +9,20 @@ package br.com.spiderbot.fonema;
  * @see https://github.com/LVRodrigues/jfonemabr/blob/main/docs/NOVOB.pdf
  */
 public class Fonema {
-    
+
+    /**
+     * Construtor oculto, tornando uma classe utilitária.
+     */
+    private Fonema() {
+    }
+
+    /**
+     * Processa um texto aplicando as regras do algorítimo BuscarBR.
+     * 
+     * @param text Texto para extrair os fonemas.
+     * @return Fonemas.
+     */
+    public static String process(String text) {
+        return null;
+    }
 }
--- src/test/java/br/com/spiderbot/fonema/FonemaTest.java
+++ src/test/java/br/com/spiderbot/fonema/FonemaTest.java
@@ -1,5 +1,10 @@
 package br.com.spiderbot.fonema;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 /**
  * Testes unitários para {@link Fonema}.
  * 
@@ -8,5 +13,56 @@ package br.com.spiderbot.fonema;
  * @version $Branch$
  */
 public class FonemaTest {
+
+    /**
+     * Matriz de palavras para tests fonéticos.
+     */
+    private static final String[] INPUTS = {
+        "Luciano",
+        "Que a força esteja com você",
+        "Casamento",
+        "Liquidação",
+        "Horizonte",
+        "Horizontal",
+        "Melhoria",
+        "Propagação"
+    };
+
+    /**
+     * Matriz de resultados esperados para os testes fonéticos,
+     * conforme a matriz INPUTS.
+     */
+    private static final String[] EXPECTEDS = {
+        "RSM",
+        "K FRK TJ K V",
+        "KSMT",
+        "RKDK",
+        "RSMT",
+        "RSMT",
+        "MR",
+        "PGK"
+    };
     
+    /**
+     * Teste de processamento de fonemas, com palavras e fonemas conhecidos.
+     * <p>
+     * A matriz INPUTS possui os mesmos índices da matriz EXPECTEDS.
+     */
+    @Test
+    public void testProcess() {
+        for (int i = 0; i < INPUTS.length; i++) {
+            String result = Fonema.process(INPUTS[i]);
+            assertEquals(EXPECTEDS[i], result, String.format("Falha ao processar a palavra %s.", INPUTS[i]));
+        }
+    }
+
+    /**
+     * Teste de fonemas com palavras escritas de modo errado, mas sonoramente parecidas.
+     */
+    @Test
+    public void testFejaum() {
+        String feijao = Fonema.process("Feijão");
+        String fejaum = Fonema.process("Fejaum");
+        assertEquals(feijao, fejaum, "Falha no teste do feijão.");
+    }
 }
