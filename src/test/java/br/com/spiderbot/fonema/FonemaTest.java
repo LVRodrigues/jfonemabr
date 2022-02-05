074f7b06e00fac7801103192760b4ee272b622dc
Luciano Vieira Rodrigues <lvrodriguesline@gmail.com>
2022-02-04 23:19:14 -0300

    Preparando testes unitários.

---
+++ src/test/java/br/com/spiderbot/fonema/FonemaTest.java
@@ -0,0 +1,12 @@
+package br.com.spiderbot.fonema;
+
+/**
+ * Testes unitários para {@link Fonema}.
+ * 
+ * @author $Author$
+ * @author $Committer$
+ * @version $Branch$
+ */
+public class FonemaTest {
+    
+}
